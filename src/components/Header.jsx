031303aee0f9e8f1d470514f8bebbcaf07ce713c
xenon <pmcruz@hermenautas.com>
2022-10-13 11:09:51 +0200

    dev 2.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div><h1>ESTE ES EL HEADER</h1></div>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div><h1>Este es el nav</h1></div>
+  )
+}
+
+export default Nav
